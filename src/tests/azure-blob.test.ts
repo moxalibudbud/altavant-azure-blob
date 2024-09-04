8bf80e87db50f5161a4ccb0a3f07226e4f12b8e6
John <moxalibudbud@gmail.com>
2024-09-04 14:37:41 +0400

    refactor test

--- src/tests/azure-blob.test.ts
+++ src/tests/azure-blob.test.ts
@@ -1,17 +1,38 @@
 import 'dotenv/config';
-import { test, expect, describe } from '@jest/globals';
+import { jest, test, expect, describe } from '@jest/globals';
 import { BlobServiceClient, ContainerClient, StorageSharedKeyCredential } from '@azure/storage-blob';
-import { Container, serviceClient, AzureBlobContainers, ServiceClientCredentials } from '..';
+import { ServiceClientCredentials, AzureBlobContainers } from '../index';
+import { serviceClient } from '../service-client';
+import { Container } from '../container';
+
+// Mocking the @azure/storage-blob module
+jest.mock('@azure/storage-blob', () => ({
+  BlobServiceClient: jest.fn(),
+  ContainerClient: jest.fn(),
+  StorageSharedKeyCredential: jest.fn(),
+}));
+
+jest.mock('../service-client', () => ({
+  serviceClient: jest.fn().mockImplementation(() => {
+    return new BlobServiceClient('url');
+  })
+}));
+
+jest.mock('../Container', () => ({
+  Container: jest.fn().mockImplementation(() => ({
+    serviceClient: new BlobServiceClient('url'),
+    containerClient: new ContainerClient('url'),
+  }))
+}));
 
 describe('Azure Blob Storage library tests', () => {
   
   test('Service Client should pass with arguments', () => {
+    const AZURE_BLOB_STORAGE_ACCOUNT_NAME = 'mockAccountName';
+    const AZURE_BLOB_STORAGE_ACCOUNT_KEY = 'mockAccountKey';
     const credentials: ServiceClientCredentials = {
-      credentials: new StorageSharedKeyCredential(
-        process.env.AZURE_BLOB_STORAGE_ACCOUNT_NAME as string,
-        process.env.AZURE_BLOB_STORAGE_ACCOUNT_KEY as string
-      ),
-      accountName: process.env.AZURE_BLOB_STORAGE_ACCOUNT_NAME as string
+      credentials: new StorageSharedKeyCredential(AZURE_BLOB_STORAGE_ACCOUNT_NAME, AZURE_BLOB_STORAGE_ACCOUNT_KEY),
+      accountName: AZURE_BLOB_STORAGE_ACCOUNT_NAME,
     };
 
     const client = serviceClient(credentials)
